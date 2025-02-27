5ccd82ef87794c66de5ef209b98acabbb26704fb
Cintyabio <cintyabio@gmail.com>
2025-02-27 14:25:23 -0300

    deleting requested files on Greeting.java and on HelloGraphQLResource.java

--- src/main/java/br/com/pedr0limpio/samples/HelloGraphQLResource.java
+++ src/main/java/br/com/pedr0limpio/samples/HelloGraphQLResource.java
@@ -1,15 +1,3 @@
-package br.com.pedr0limpio.samples;
-
-import org.eclipse.microprofile.graphql.*;
 //TODO[#8]: Delete/remove this file and package samples
-@GraphQLApi
-public class HelloGraphQLResource {
 
-    @Query
-    @Description("Say hello")
-    public Greeting sayHello(@DefaultValue("Renan") String name,
-                             @DefaultValue("Silva") String surname) {
-        return new Greeting(name, surname);
-    }
-}
 
