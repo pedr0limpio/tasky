6f2eb14d386733c3ba7d8baa55019d194336017d
silrenan <rcs.593@bb.com.br>
2025-05-22 13:02:37 -0300

    implement getById method to fetch a task by ID and update related tests

--- src/main/java/br/com/pedr0limpio/models/Task.java
+++ src/main/java/br/com/pedr0limpio/models/Task.java
@@ -19,7 +19,13 @@ public class Task {
         return id;
     }
 
-    public void setId(int id) { this.id = id;}
+    public Task() {
+        // Default constructor required for frameworks
+    }
+
+    public Task(int id) {
+        this.id = id;
+    }
 
     public String getDescription() {
         return description;
